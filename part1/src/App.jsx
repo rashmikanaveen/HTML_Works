b7b82e9ad6f94322fd64db3acde4fb49beecb622
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-07-13 19:06:41 +0530

    1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,32 @@
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+const Hello = (props) => {
 
+  console.log(props)
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
       <p>
-        {a} plus {b} is {a + b}
+
+        Hello {props.name}, you are {props.age} years old
       </p>
     </div>
   )
 }
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
