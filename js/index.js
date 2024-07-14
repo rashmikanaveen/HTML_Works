ae80c2e5b23c4f54407269da9715e5eb46e379f2
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-07-14 12:23:34 +0530

    learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
-const t = [1, 2, 3, 4, 5]
+const object1 = {
+  name: 'Arto Hellas',
+  age: 35,
+  education: 'PhD',
+}
 
-const [first, second, ...rest] = t
+const object2 = {
+  name: 'Full Stack web application development',
+  level: 'intermediate studies',
+  size: 5,
+}
 
-console.log(first, second)  // 1, 2 is printed
-console.log(rest)          // [3, 4, 5] is printed
+const object3 = {
+  name: {
+    first: 'Dan',
+    last: 'Abramov',
+  },
+  grades: [2, 3, 5, 3],
+  department: 'Stanford University',
+}
+console.log(object1.name)         // Arto Hellas is printed
+const fieldName = 'age'
+console.log(object1[fieldName])    // 35 is printed
