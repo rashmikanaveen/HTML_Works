625ff6ece4d5d9a73a8cf81d2c5af1ea3ba1615c
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-07-14 13:07:37 +0530

    learn function

--- js/index.js
+++ js/index.js
@@ -1,23 +1,23 @@
-const object1 = {
-  name: 'Arto Hellas',
-  age: 35,
-  education: 'PhD',
+const sum = (p1, p2) => {
+  console.log(p1)
+  console.log(p2)
+  return p1 + p2
 }
 
-const object2 = {
-  name: 'Full Stack web application development',
-  level: 'intermediate studies',
-  size: 5,
+const square1 = p => {
+  console.log(p)
+  return p * p
 }
+const square = p => p * p
+const t = [1, 2, 3]
+const tSquared = t.map(p => square(p))
 
-const object3 = {
-  name: {
-    first: 'Dan',
-    last: 'Abramov',
-  },
-  grades: [2, 3, 5, 3],
-  department: 'Stanford University',
+// tSquared is now [1, 4, 9]
+function product(a, b) {
+  return a * b
 }
-console.log(object1.name)         // Arto Hellas is printed
-const fieldName = 'age'
-console.log(object1[fieldName])    // 35 is printed
+const average=(a,b)=>(a+b)/2
+
+const result = average(2, 6)
+console.log(result)
+// result is now 12
