9a89386b445a4ebd1661e9aa11cd7b0ebd0f9c4f
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-07-14 12:14:11 +0530

    test

--- js/index.js
+++ js/index.js
@@ -1,10 +1,6 @@
-const t = [1, -1, 3]
+const t = [1, 2, 3, 4, 5]
 
-t.push(5)
+const [first, second, ...rest] = t
 
-console.log(t.length) // 4 is printed
-console.log(t[1])     // -1 is printed
-
-t.forEach(value => {
-  console.log(value)  // numbers 1, -1, 3, 5 are printed, each on its own line
-})                    
+console.log(first, second)  // 1, 2 is printed
+console.log(rest)          // [3, 4, 5] is printed
