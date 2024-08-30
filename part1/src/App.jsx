acc04c40ba221d2405629fc986674f83bff1ed5b
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-08-30 23:23:19 +0530

    lern Controlled component

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,15 +1,46 @@
-import Note  from './components/Note'
+import { useState } from 'react'
+import Note from './components/Note'
 
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
 
+  const [newNote, setNewNote] = useState('a new note...')
 
-const App = () => {
-  console.log("hi_3")
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+    
+  
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
   return (
     <div>
-      <Note />
-      
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+
+        <input value={newNote}  onChange={handleNoteChange} />
+        <p>You typed: {newNote}</p>
+        <button type="submit">save</button>
+      </form>   
     </div>
-  );
+  )
 }
 
-export default App
+export default App 
--- part1/src/components/Note.jsx
+++ part1/src/components/Note.jsx
@@ -1,38 +1,14 @@
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
 
-const Note2 = (Notes) => {
-  console.log("Hi_2")
-    return (
-      
-      <div>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
-      </div>
-    )
-  }
+
+const Note = ({ note }) => {
+  
+  return (
+    <li>{note.content}</li>
+  )
+}
+
   
 
 
-const Note = () => {
-  console.log("hi_1")
-    return (
-      <Note2 note={notes} />
-    )
-  }
-  
+
   export default Note
