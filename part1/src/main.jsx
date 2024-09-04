f1386b31400d2faef52ca84daef20e68fc097297
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-05 01:14:35 +0530

    get data from local server  and show in browser

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,8 +1,8 @@
+import ReactDOM from 'react-dom/client'
 import axios from 'axios'
+import App from './App'
 
-axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    console.log(notes)
-  })
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
