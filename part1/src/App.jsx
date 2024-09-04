0397aab155f8bb638822b7415ce6a1ecc9fbc213
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-05 01:32:47 +0530

    feat: Fetch and display notes from server in App component

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
 
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+  
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    }
+  
+    const promise = axios.get('http://localhost:3001/notes')
+    promise.then(eventHandler)
+  }, [])
+  
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
