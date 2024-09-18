b322070523c488fe29f502d5b3a58452a158c968
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-18 08:35:42 +0530

    Refactor App component to add form for adding employee information

--- CRUDApp/client/src/App.js
+++ CRUDApp/client/src/App.js
@@ -8,21 +8,28 @@ function App() {
   const[Country, setCountry] = useState('');
   const[wage, setWage] = useState(0);
   const[Position, setPosition] = useState('');
+
+
+const displayName=()=>{
+  console.log(name+Age+Country+wage+Position);
+  
+}
+
   return (
     <div className="App">
-        <div className='information'>
+        <form className='information'>
         <label>Enter Name:</label>
-        <input type='text' onChange={(event)=>{setName(event.target.value)}}></input>
+        <input type='text' onChange={(event)=>{setName(event.target.value)}} required />
         <label>Enter Age:</label>
-        <input type='number' onChange={(event)=>{setAge(event.target.value)}}></input>
+        <input type='number' onChange={(event)=>{setAge(event.target.value)}} required></input>
         <label>Position:</label>
-        <input type='text' onChange={(event)=>{setPosition(event.target.value)}}></input>
+        <input type='text' onChange={(event)=>{setPosition(event.target.value)}} required></input>
         <label>Country:</label>
-        <input type='text' onChange={(event)=>{setCountry(event.target.value)}}></input>
+        <input type='text' onChange={(event)=>{setCountry(event.target.value)}} required></input>
         <label>Wage(year):</label>
-        <input type='number' onChange={(event)=>{setWage(event.target.value)}}></input>
-        <button>Add Employee</button>
-        </div>
+        <input type='number' onChange={(event)=>{setWage(event.target.value)}} required></input>
+        <button >Add Employee</button>
+        </form>
     </div>
   );
 }
