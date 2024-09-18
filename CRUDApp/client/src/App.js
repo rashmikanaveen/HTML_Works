e00ea44107a062f6d2ec841961c4378d7f1f762e
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-09-18 08:28:01 +0530

    Refactor styling and add form for adding employee information

--- CRUDApp/client/src/App.js
+++ CRUDApp/client/src/App.js
@@ -1,10 +1,28 @@
 
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const[name, setName] = useState('');
+  const[Age, setAge] = useState(0);
+  const[Country, setCountry] = useState('');
+  const[wage, setWage] = useState(0);
+  const[Position, setPosition] = useState('');
   return (
     <div className="App">
-      Hello My name is Rashmika
+        <div className='information'>
+        <label>Enter Name:</label>
+        <input type='text' onChange={(event)=>{setName(event.target.value)}}></input>
+        <label>Enter Age:</label>
+        <input type='number' onChange={(event)=>{setAge(event.target.value)}}></input>
+        <label>Position:</label>
+        <input type='text' onChange={(event)=>{setPosition(event.target.value)}}></input>
+        <label>Country:</label>
+        <input type='text' onChange={(event)=>{setCountry(event.target.value)}}></input>
+        <label>Wage(year):</label>
+        <input type='number' onChange={(event)=>{setWage(event.target.value)}}></input>
+        <button>Add Employee</button>
+        </div>
     </div>
   );
 }
